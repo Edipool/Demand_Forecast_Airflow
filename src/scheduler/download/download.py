5677e160ee48c9e46c246d2578ceb74c41b2bb4e
Eduard Poliakov <ya.polykov@gmail.com>
2023-09-27 18:58:50 +0300

    add download_and_log function in download.py

--- src/scheduler/download/download.py
+++ src/scheduler/download/download.py
@@ -10,27 +10,23 @@ handler = logging.StreamHandler(sys.stdout)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
+def download_and_log(s3_bucket: str, remote_path: str, output_path: str, file_name: str):
+    remote_file_path = f"{remote_path}/{file_name}"
+    output_local_path = f"{output_path}/{file_name}"
+
+    download_file(bucket_name=s3_bucket, remote_path=remote_file_path, local_path=output_local_path,)
+    logger.info(f"remote_path: {remote_file_path}")
+    logger.info(f"output_local_path: {output_local_path}")
 
 @click.command("download")
 @click.option("--s3-bucket")
 @click.option("--remote-path")
 @click.option("--output-path")
 def download_dataset(s3_bucket: str, remote_path: str, output_path: str):
-    remote_path_demand_orders = f"{remote_path}/demand_orders.csv"
-    output_local_path_demand_orders = f"{output_path}/demand_orders.csv"
-    remote_path_demand_orders_status = f"{remote_path}/demand_orders_status.csv"
-    output_local_path_demand_orders_status = f"{output_path}/demand_orders_status.csv"
-
     os.makedirs(output_path, exist_ok=True)
-    # Download demand_orders.csv
-    download_file(bucket_name=s3_bucket, remote_path=remote_path_demand_orders, local_path=output_local_path_demand_orders,)
-    logger.info(f"remote_path: {remote_path}")
-    logger.info(f"output_local_path: {output_local_path_demand_orders}")
-    # Download demand_orders_status.csv
-    download_file(bucket_name=s3_bucket, remote_path=remote_path_demand_orders_status, local_path=output_local_path_demand_orders_status,)
-    logger.info(f"remote_path: {remote_path}")
-    logger.info(f"output_local_path: {output_local_path_demand_orders_status}")
-
+    # Download demand_orders.csv and demand_orders_status.csv
+    download_and_log(s3_bucket, remote_path, output_path, "demand_orders.csv")
+    download_and_log(s3_bucket, remote_path, output_path, "demand_orders_status.csv")
 
 if __name__ == "__main__":
     download_dataset()
