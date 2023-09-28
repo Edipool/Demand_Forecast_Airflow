ecf53018bafb59e868b18ac61479142abff77ad3
Eduard Poliakov <ya.polykov@gmail.com>
2023-09-28 09:51:01 +0300

    add successful completion of the stage split

--- src/scheduler/split/split.py
+++ src/scheduler/split/split.py
@@ -2,7 +2,7 @@ import os
 import logging
 import sys
 import click
-from make_dataset.make_dataset import read_data, split_train_val_data
+from make_dataset.make_dataset import read_data, split_train_test
 
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(sys.stdout)
@@ -13,20 +13,21 @@ logger.addHandler(handler)
 @click.command("split")
 @click.option("--input-dir")
 @click.option("--output-dir")
-@click.option("--test-size")
-def split(input_dir: str, output_dir: str, test_size: str):
+@click.option("--test_days")
+def split(input_dir: str, output_dir: str, test_days: int):
     logger.info(f"input_dir: {input_dir}")
     logger.info(f"output_dir: {output_dir}")
-    logger.info(f"test_size: {test_size}")
+    logger.info(f"test_size: {test_days}")
 
     os.makedirs(output_dir, exist_ok=True)
 
-    df = read_data(os.path.join(input_dir, "processed.csv"))
+    df = read_data(os.path.join(input_dir, "features_targets.csv"))
 
-    train, test = split_train_val_data(df, test_size=float(test_size))
+    train, test = split_train_test(df, test_days=int(test_days))
 
     train.to_csv(os.path.join(output_dir, "train.csv"), index=False)
     test.to_csv(os.path.join(output_dir, "test.csv"), index=False)
+    logger.info(f"Split data complete!")
 
 
 if __name__ == "__main__":
